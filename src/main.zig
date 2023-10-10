a932efa47de7e03c870dfbd2f992292a87219780
Ivan Canosa <ivanadrielcl@hotmail.es>
2023-10-10 05:33:12 +0200

    Added raylib dep

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
-const std = @import("std");
+const raylib = @import("raylib");
 
-pub fn main() !void {
-    std.debug.print("Hello world\n", .{});
+pub fn main() void {
+    raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = false });
+    raylib.InitWindow(800, 800, "hello world!");
+    raylib.SetTargetFPS(60);
+
+    defer raylib.CloseWindow();
+
+    while (!raylib.WindowShouldClose()) {
+        raylib.BeginDrawing();
+        defer raylib.EndDrawing();
+
+        raylib.ClearBackground(raylib.BLACK);
+        raylib.DrawFPS(10, 10);
+
+        raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
+    }
 }
